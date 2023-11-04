bcc7246b4c55c3632afa955c23ec4227947c1487
somespecialone <tkachenkodmitriy@yahoo.com>
2023-11-04 14:29:54 +0100

    v2.0.0. Attempt to fix github caching.

--- middleware/cache.ts
+++ middleware/cache.ts
@@ -1,3 +1,4 @@
 export default eventHandler((event) => {
-  setResponseHeaders(event, { 'Cache-Control': 'no-store' })
+  // no-store isn't enough for github proxy servers (masking markdown badge source urls)
+  setResponseHeaders(event, { 'Cache-Control': 'max-age=0, no-cache, no-store, must-revalidate' })
 })
